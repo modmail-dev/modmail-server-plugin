d8d719b2dfc5e538e775918d29555c9b63fd8142
Taku <45324516+Taaku18@users.noreply.github.com>
2022-12-19 04:11:58 -0800

    Add future import

--- auto_delete_webhook_messages/auto_delete_webhook_messages.py
+++ auto_delete_webhook_messages/auto_delete_webhook_messages.py
@@ -1,7 +1,7 @@
 """
 Automatically delete the annoying [modmail] None on development, etc. messages in #github.
 """
-
+from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
