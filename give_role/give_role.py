8edb1bbafcb808e957b55b01f92e0f9a45833065
Taku <45324516+Taaku18@users.noreply.github.com>
2023-07-25 17:38:57 -0700

    Add give_role_plugin

---
+++ give_role/give_role.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import discord
+from discord.ext import commands
+
+from core import checks
+from core.models import PermissionLevel, getLogger
+from core.utils import human_join
+
+logger = getLogger(__name__)
+
+
+class GiveRole(commands.Cog):
+    """A plugin that gives the thread recipient a role."""
+
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
+    @checks.thread_only()
+    async def giverole(self, ctx, role: discord.Role):
+        """
+        Gives the thread recipient a role. DANGEROUS COMMAND: see description.
+
+        This command is dangerous because it does not check for permissions.
+        Anyone who can use this command can give themselves, or anyone else, any role.
+        """
+        recipients = ctx.thread.recipients
+        try:
+            for recipient in recipients:
+                await recipient.add_roles(role, reason=f"Role given by {ctx.author} in thread {ctx.thread}")
+        except Exception as e:
+            await ctx.send(f"Error giving role: {e}")
+        await ctx.send(f"{role.name} given to {human_join([r.name for r in recipients])}.")
+
+
+async def setup(bot):
+    await bot.add_cog(GiveRole())
