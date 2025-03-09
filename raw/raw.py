8ef2055a3d6094fcf7bcd1ed6784db46d80e90c3
Stephen <48072084+StephenDaDev@users.noreply.github.com>
2025-03-09 18:24:32 -0400

    Add raw plugin
    * Add plugin developed by community member to allow use in Modmail Support server.
    * Add plugin help message which includes credit of original author.

---
+++ raw/raw.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+from core import checks
+from core.models import PermissionLevel
+
+
+class EmbedRaw(commands.Cog):
+"""Simple Plugin to print embeded text with copyable Markdown.
+
+Credit to [matrix2113](https://github.com/matrix2113) on GitHub."""
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    async def raw(self, ctx, message_id: int=None):
+        if message_id is None:
+            return await ctx.send("Please provide a message ID")
+        
+        try:
+            msg = await ctx.fetch_message(message_id)
+        except Exception as e:
+            print(str(e))
+        msgg = msg.embeds[0]
+        await ctx.send(f"```{msgg.description}```")
+        
+
+async def setup(bot):
+    await bot.add_cog(EmbedRaw(bot))
